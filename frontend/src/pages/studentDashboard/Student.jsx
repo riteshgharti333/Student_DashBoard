dc8c943a11a5c180be8f5ecce4b324cd8d569d49
hp <you@example.com>
2023-09-01 22:23:41 +0530

    changes some ui

--- frontend/src/pages/studentDashboard/Student.jsx
+++ frontend/src/pages/studentDashboard/Student.jsx
@@ -1,16 +1,11 @@
 import "./Student.scss";
-import DashboardHeader from "../../component/DashboardHeader/DashboardHeader";
-import DashboardSidebar from "../../component/DashboardSidebar/DashboardSidebar";
 import DashBoardCards from "../../component/DashBoardCards/DashBoardCards";
 
 const Student = () => {
   return (
     <div className="studentDashboard">
-      <div className="mainSection">
-        {/* <DashboardSidebar /> */}
         <DashBoardCards />
       </div>
-    </div>
   );
 };
 
